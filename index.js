c7da028f9f1ab79b7100a1b0584dbd9161240b5f
shaurya <shauryadhaka3@gmail.com>
2025-01-08 12:21:39 +0530

    added Production url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: "http://localhost:4200",
+    origin: ["http://localhost:4200",process.env.PRODUCTION_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
